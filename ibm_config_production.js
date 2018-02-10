1d4dea0439daba92b59e3d3ced7c62e454042d7b
acashmkj <acash.mkj@gmail.com>
2018-02-11 03:10:11 +0530

    twitter

---
+++ ibm_config_production.js
@@ -0,0 +1,6 @@
+module.exports = {
+  "url": 'https://gateway.watsonplatform.net/tone-analyzer/api',
+  "username": '',
+  "password": '',
+  "version_date": "2016-05-19"
+};
---
+++ twitter_config_production.js
@@ -0,0 +1,9 @@
+module.exports = {
+ // consumer_key: process.env.TWITTER_KEY,
+  consumer_key: '',
+  //consumer_secret: process.env.TWITTER_SECRET,
+  consumer_secret: '',
+  bearer_token: ''
+};
+
+//process.env.BEARER_TOKEN
